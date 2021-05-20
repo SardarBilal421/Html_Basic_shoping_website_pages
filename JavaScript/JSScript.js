2c04ab29077962bd227740a261150c0958abdf6b
SardarBilal142 <rockeykhan142@gmail.com>
2021-05-20 13:26:17 +0500

    first commit

---
+++ JavaScript/JSScript.js
@@ -0,0 +1,21 @@
+
+function NewsAlert() {
+ alert("SALE SALE SALE")
+}
+function mOver(obj){
+ obj.innerHTML = "Click Me! ";
+}
+function mOut(obj){
+ obj.innerHTML = "Explore Now! "
+}
+function bottomFunction() {
+ document.body.scrollHeight= 0; // For Safari
+ document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+function Cartf(obj){
+ obj.innerHTML = "Add To Cart "
+
+}function BuyN(obj){
+ obj.innerHTML = "BUY NOW! "
+
+}
